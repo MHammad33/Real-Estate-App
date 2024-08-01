1c10ccfd3f247b25272b5cc27eb376f60891cf81
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-01 22:59:28 +0530

    Add cors library

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import postRouter from "./routes/post.routes";
 import authRouter from "./routes/auth.routes";
+import config from "./utils/config";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+	cors({
+		origin: config.CLIENT_URL,
+		credentials: true,
+	})
+);
 
 // Routes
 app.use("/api/v1/posts", postRouter);
--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -4,9 +4,11 @@ config();
 const PORT = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 const JWT_LIFETIME = process.env.JWT_LIFETIME || "1d";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173/";
 
 export default {
 	PORT,
 	JWT_SECRET,
 	JWT_LIFETIME,
+	CLIENT_URL,
 };
