17593e0467c2e38a2a952efb02ed1d5e837c8d90
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-02 15:23:25 +0530

    Add Search Bar in Homepage

--- client/src/components/searchBar/SearchBar.tsx
+++ client/src/components/searchBar/SearchBar.tsx
@@ -1,7 +1,54 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
+
 const SearchBar: FC = () => {
-	return <div>SearchBar</div>;
+	const [query, setQuery] = useState({
+		type: "buy",
+		location: "",
+		minPrice: 0,
+		maxPrice: 10000000,
+	});
+
+	const switchType = (typeValue: string) => {
+		setQuery((prev) => ({ ...prev, type: typeValue }));
+	};
+
+	return (
+		<div className="searchBar">
+			<div className="type">
+				{types.map((type) => (
+					<button
+						className={query.type === type ? "active" : ""}
+						key={type}
+						onClick={() => switchType(type)}
+					>
+						{type}
+					</button>
+				))}
+			</div>
+			<form>
+				<input type="text" name="location" placeholder="City Location" />
+				<input
+					type="number"
+					name="minPrice"
+					min={0}
+					max={10000000}
+					placeholder="Min Price"
+				/>
+				<input
+					type="number"
+					name="maxPrice"
+					min={0}
+					max={10000000}
+					placeholder="Max Price"
+				/>
+				<button>
+					<img src="/search.png" alt="Search" />
+				</button>
+			</form>
+		</div>
+	);
 };
 export default SearchBar;
