a23c2e563cca01d24ab999ea9c669d2857b43cac
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-02 12:43:51 +0500

    Add Homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Navbar } from "./components";
 import "./layout.scss";
+import { HomePage } from "./pages";
 
 function App() {
 	return (
 		<div className="layout">
 			<Navbar />
+			<div className="content">
+				<HomePage />
+			</div>
 		</div>
 	);
 }
---
+++ client/src/pages/homePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import "./homePage.scss";
+
+const HomePage: FC = () => {
+	return <div>HomePage</div>;
+};
+export default HomePage;
