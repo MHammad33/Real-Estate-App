6691a4e4c0fb7bcb7041f7da8f002fc251340f19
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-02 16:51:48 +0500

    Resolve issue of files swapped

--- client/src/pages/listPage/ListPage.tsx
+++ client/src/pages/listPage/ListPage.tsx
@@ -1,7 +1,23 @@
 import { FC } from "react";
 import "./listPage.scss";
+import { Card, Filter } from "../../components";
+import { listData } from "../lib/dummyData";
 
 const ListPage: FC = () => {
-	return <div>ListPage</div>;
+	const data = listData;
+
+	return (
+		<div className="listPage">
+			<div className="listContainer">
+				<div className="wrapper">
+					<Filter />
+					{data.map((item) => (
+						<Card key={item.id} />
+					))}
+				</div>
+			</div>
+			<div className="mapContainer">Map</div>
+		</div>
+	);
 };
 export default ListPage;
--- client/src/pages/singlePage/SinglePage.tsx
+++ client/src/pages/singlePage/SinglePage.tsx
@@ -1,22 +1,7 @@
 import { FC } from "react";
 import "./singlePage.scss";
-import { Card, Filter } from "../../components";
-import { listData } from "../lib/dummyData";
 
 const SinglePage: FC = () => {
-	const data = listData;
-	return (
-		<div className="singlePage">
-			<div className="listContainer">
-				<div className="wrapper">
-					<Filter />
-					{data.map((item) => (
-						<Card key={item.id} />
-					))}
-				</div>
-			</div>
-			<div className="mapContainer">Map</div>
-		</div>
-	);
+	return <div className="singlePage">SinglePage</div>;
 };
 export default SinglePage;
