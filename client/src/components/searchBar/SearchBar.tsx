925a22c0830a8a4a22fa490e88a4b33ea77aa7ad
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-08-02 13:02:55 +0500

    Add Homepage Layout

---
+++ client/src/components/searchBar/SearchBar.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import "./searchBar.scss";
+
+const SearchBar: FC = () => {
+	return <div>SearchBar</div>;
+};
+export default SearchBar;
--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -1,7 +1,41 @@
 import { FC } from "react";
 import "./homePage.scss";
+import { SearchBar } from "../../components";
 
 const HomePage: FC = () => {
-	return <div>HomePage</div>;
+	return (
+		<div className="homepage">
+			<div className="textContainer">
+				<div className="wrapper">
+					<h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+						praesentium, assumenda accusantium facere pariatur delectus nesciunt
+						voluptatibus dolores inventore ipsam earum id reiciendis
+						perspiciatis temporibus debitis fuga dicta commodi quibusdam
+						deleniti? Doloremque, velit.
+					</p>
+					<SearchBar />
+					<div className="boxes">
+						<div className="box">
+							<h1>16+</h1>
+							<h2>Years of Experience</h2>
+						</div>
+						<div className="box">
+							<h1>200</h1>
+							<h2>Award Gained</h2>
+						</div>
+						<div className="box">
+							<h1>1200+</h1>
+							<h2>Property Ready</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="imgContainer">
+				<img src="/bg.png" alt="" />
+			</div>
+		</div>
+	);
 };
 export default HomePage;
